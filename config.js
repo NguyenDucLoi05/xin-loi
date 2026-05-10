23f1c3b04d4e105f5087bd88175c1fc484c3c4d4
NguyenDucLoi05 <143474829+NguyenDucLoi05@users.noreply.github.com>
2026-05-10 14:03:36 +0700

    Update CONFIG object with new messages and titles

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "Loi xin loi",
+    introTitle: 'Hny ',
+    introDesc: `Gui hny,
+    bbi biet... thoi gian qua bbi da khien hny buon nhieu hon la vui
+    co nhung chuyen bbi khong giai thich kip, co nhung dieu bbi lam ma chinh bbi nhin lai cung thay bbi tre con.
+    khong dang voi su diu dang ma hny da danh cho bbi. Bbi khong co y lam ton thuong hny dau!`,
+    btnIntro:'next',
+    title: 'chi la bbi khong gioi noi ra, khong gioi an ui, cang khong gioi trong viec khien nguoi minh thuong cam thay an tam. Bbi vo tam that nhi?, va sau ngay hqua chinh bbi cung thay ghet bbi vi dieu do.Moi lan hny im lang ay, bbi chi biet ngoi nhin tin nhan chua duoc tra loi hoac ngoi nhin hny quay lung voi bbi roi tu hoi lieu bbi da lam gi sai them nua. Bbi so cam giac hny that vong, so anh mat buon cua hny, so caicach ma hny quay di khi bbi chua kip giu lai nua, bbi chua 1 giay nao muon roi xa hny ca, nhung ma trong tinh cam khong phai muon la duoc, hny co quyen buon co quyen gian cung vi bbi da khong lam tron vai nguoi hny tin tuong.',
+    desc: 'Nay bbi khong biet phai noi sao nua nhung ma bbi chi muon noi hny rang: bbi xin loi bbi biet loi cua bbi rui, bbi cung dang hoc cach de khong khien hny ton thuong nhieu nhu nay nua. Neu co the ban phat chut long tu bi cho bbi mot chut nhe long tu hny nha, 1 chut tha thu du chi nho thui de bbi biet bbi con co hoi sua sai. Neu hny doc toi day rui thi cho bbi xin loi nha dung gian bbi nua nha, bbi that su khong muon bien minh thanh ly do khien hny buon nhu dau T-T...',
+    btnYes: 'THA THU',
+    btnNo: 'COOK',
+    question: 'Khong biet hny co con thuong bbi khong?',
+    btnReply: 'Thuong cc',
+    reply: 'Thuong qq',
+    mess: 'Cam on vi da thuong bbi nghen!',
+    messDesc: 'BBI THUONG HNY NHIEU LAM!!! DUNG VI CHUYEN HOM QUA MA BUON BBI NGHEN!!!',
+    btnAccept: 'Ib cho bbi nghen neu doc het va da tha thu nghen!',
+    messLink: 'https://www.facebook.com/loinyguenduc11112005' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
